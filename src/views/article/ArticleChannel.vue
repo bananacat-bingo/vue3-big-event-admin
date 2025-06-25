4d86758cc53fbb4287ebf07f26bd6c3a48e94b12
香蕉喵 <3461699696@qq.com>
2025-06-25 16:44:24 +0800

    添加文章分类模块

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -1,1 +1,72 @@
-<template>频道管理</template>
+<script setup>
+import { artDelChannelService, artGetChannelsService } from '@/api/article'
+import PageContainer from '@/components/PageContainer.vue'
+import ChannelEdit from './components/ChannelEdit.vue'
+import { Edit, Delete } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+
+const channelList = ref([])
+const loading = ref(false)
+const dialog = ref()
+const getChannelList = async () => {
+  loading.value = true
+  const res = await artGetChannelsService()
+  channelList.value = res.data
+  loading.value = false
+}
+getChannelList()
+const onEditChannel = (row) => {
+  dialog.value.open(row)
+}
+const onDelChannel = async (row) => {
+  await ElMessageBox.confirm('你确定要删除该分类吗', '温馨提示', {
+    type: 'warning',
+    confirmButtonText: '确认',
+    cancelButtonText: '取消'
+  })
+  await artDelChannelService(row.id)
+  ElMessage.success('删除成功')
+  getChannelList()
+}
+const onAddChannel = () => {
+  dialog.value.open({})
+}
+const onSuccess = () => getChannelList()
+</script>
+
+<template>
+  <PageContainer title="文章分类">
+    <template #extra>
+      <el-button @click="onAddChannel" type="primary">添加分类</el-button>
+    </template>
+    <el-table v-loading="loading" :data="channelList" style="width: 100%">
+      <el-table-column type="index" label="序号" width="100"></el-table-column>
+      <el-table-column prop="cate_name" label="分类名称"></el-table-column>
+      <el-table-column prop="cate_alias" label="分类别名"></el-table-column>
+      <el-table-column label="操作" width="150">
+        <template #default="{ row, $index }">
+          <el-button
+            :icon="Edit"
+            circle
+            plain
+            type="primary"
+            @click="onEditChannel(row, $index)"
+          ></el-button
+          ><el-button
+            :icon="Delete"
+            circle
+            plain
+            type="danger"
+            @click="onDelChannel(row, $index)"
+          ></el-button>
+        </template>
+      </el-table-column>
+      <template #empty>
+        <el-empty description="暂无数据"></el-empty>
+      </template>
+    </el-table>
+    <ChannelEdit ref="dialog" @success="onSuccess"></ChannelEdit>
+  </PageContainer>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -1,1 +1,13 @@
-<template>文章管理</template>
+<script setup>
+import PageContainer from '@/components/PageContainer.vue'
+</script>
+
+<template>
+  <PageContainer title="文章管理">
+    <template #extra>
+      <el-button type>添加文章</el-button>
+    </template>
+  </PageContainer>
+</template>
+
+<style lang="scss" scoped></style>
