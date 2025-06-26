12994ccd1f0f4b1ef61e908be5fc29e1b9a96060
香蕉喵 <3461699696@qq.com>
2025-06-26 22:56:19 +0800

    修改基本资料功能完善

--- src/views/user/UserProfile.vue
+++ src/views/user/UserProfile.vue
@@ -1,1 +1,57 @@
-<template>个人详细</template>
+<script setup>
+import PageContainer from '@/components/PageContainer.vue'
+import { ref } from 'vue'
+import { useUserStore } from '@/stores'
+import { userUpdateInfoService } from '@/api/user'
+
+const formRef = ref()
+const {
+  user: { email, id, nickname, username },
+  getUser
+} = useUserStore()
+const form = ref({
+  id,
+  nickname,
+  email,
+  username
+})
+const rules = {
+  nickname: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 5, max: 10, message: '用户名必须是 5-10 位的字符', trigger: 'blur' }
+  ],
+  email: [
+    { required: true, message: '请输入用户邮箱', trigger: 'blur' },
+    {
+      type: 'email',
+      message: '请输入正确的邮箱格式',
+      trigger: ['blur', 'change']
+    }
+  ]
+}
+const submitForm = async () => {
+  await formRef.value.validate()
+  await userUpdateInfoService(form.value)
+  getUser()
+  ElMessage.success('修改成功')
+}
+</script>
+
+<template>
+  <PageContainer title="基本资料">
+    <el-form ref="formRef" :model="form" :rules="rules" label-width="100px">
+      <el-form-item label="登录名称">
+        <el-input v-model="form.username" disabled></el-input>
+      </el-form-item>
+      <el-form-item label="用户昵称" prop="nickname">
+        <el-input v-model="form.nickname"></el-input>
+      </el-form-item>
+      <el-form-item label="用户邮箱" prop="email">
+        <el-input v-model="form.email"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">提交修改</el-button>
+      </el-form-item>
+    </el-form>
+  </PageContainer>
+</template>
